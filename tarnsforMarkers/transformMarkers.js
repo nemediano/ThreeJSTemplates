da43de770c7886f4c548a097619de42348597baf
Jorge Garcia <jorgeg@nvidia.com>
2019-01-31 13:41:43 -0800

    Change materails in selected objects

--- tarnsforMarkers/transformMarkers.js
+++ tarnsforMarkers/transformMarkers.js
@@ -1,34 +1,49 @@
-let scene;
-let camera;
-let renderer;
-let cube;
-let orbitCtrl;
-let manipulCtrl;
-let octree;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.5, 500);
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+// Add OrbitControls so that we can pan around with the mouse.
+const orbitCtrl = new THREE.OrbitControls(camera, renderer.domElement);
+const manipulCtrl = new THREE.TransformControls( camera, renderer.domElement );
+// create octree
+const octree = new THREE.Octree( {
+    // uncomment below to see the octree (may kill the fps)
+    //scene: scene,
+    // when undeferred = true, objects are inserted immediately
+    // instead of being deferred until next octree.update() call
+    // this may decrease performance as it forces a matrix update
+    undeferred: false,
+    // set the max depth of tree
+    depthMax: Infinity,
+    // max number of objects before nodes split or merge
+    objectsThreshold: 8,
+    // percent between 0 and 1 that nodes will overlap each other
+    // helps insert objects that lie over more than one node
+    overlapPct: 0.15
+  } );
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+// Constants to dimension the scene and the number of markers
+const RADIUS_MAX = 40.0;
+const RADIUS_MAX_HALF = 20.0;
+const NUM_MARKERS = 40;
+// Globals for app logic
+const matSelected = new THREE.MeshLambertMaterial( { color: 0x00ffff} );
+let selectedGroup;
+let auxMaterials = [];
 let selectedMode = true;
 let transformMode = 0;  // 0 translate 1 scale
 
-const RADIUS_MAX = 40.0;
-const RADIUS_MAX_HALF = 20.0;
-const NUM_OBJECTS = 40;
-
-let raycaster = new THREE.Raycaster();
-let mouse = new THREE.Vector2();
-let selectedObject;
-let auxMaterial;
-
-const matSelected = new THREE.MeshLambertMaterial( { color: 0x00ffff} );
-
 function unselect () {
   // Return previous selected object to original material
-  if (selectedObject !== null) {
-    selectedObject.material = auxMaterial;
+  if (selectedGroup !== null) {
+    for (let i = 0; i < auxMaterials.length; ++i) {
+      selectedGroup.children[i].material = auxMaterials[i];
+    }
   }
-  selectedObject = null;
-  auxMaterial = null;
+  selectedGroup = null;
+  auxMaterials = [];
   if (manipulCtrl !== null) {
     manipulCtrl.enable = false;
-    //manipulCtrl.dispose();
     scene.remove(manipulCtrl);
   }
 }
@@ -39,8 +54,8 @@ function onMouseMove( event ) {
 }
 
 function onResize ( event ) {
-  let WIDTH = window.innerWidth;
-  let HEIGHT = window.innerHeight;
+  const WIDTH = window.innerWidth;
+  const HEIGHT = window.innerHeight;
   renderer.setSize(WIDTH, HEIGHT);
   camera.aspect = WIDTH / HEIGHT;
   camera.updateProjectionMatrix();
@@ -84,56 +99,37 @@ function onClick( event ) {
 }
 
 function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.5, 500);
-  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setPixelRatio( window.devicePixelRatio );
-  // create octree
-  octree = new THREE.Octree( {
-    // uncomment below to see the octree (may kill the fps)
-    //scene: scene,
-    // when undeferred = true, objects are inserted immediately
-    // instead of being deferred until next octree.update() call
-    // this may decrease performance as it forces a matrix update
-    undeferred: false,
-    // set the max depth of tree
-    depthMax: Infinity,
-    // max number of objects before nodes split or merge
-    objectsThreshold: 8,
-    // percent between 0 and 1 that nodes will overlap each other
-    // helps insert objects that lie over more than one node
-    overlapPct: 0.15
-  } );
   // Hexadecimal color (recommended)
-  let bgColor = new THREE.Color( 0xaa6622);
+  const bgColor = new THREE.Color( 0xaa6622);
   renderer.setClearColor(bgColor, 1);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
   // Create a basic meshes in random positions and add it to scene
   let marker;
-  for (let i = 0; i < NUM_OBJECTS; ++i) {
+  for (let i = 0; i < NUM_MARKERS; ++i) {
     marker = createMarker();
     // random position
     for (let j = 0; j < 3; ++j) {
       marker.position.setComponent(j, Math.random() * RADIUS_MAX - RADIUS_MAX_HALF);
     }
     // random orientation
-    let rotVec = new THREE.Vector3();
+    const rotVec = new THREE.Vector3();
     for (let j = 0; j < 3; ++j) {
       rotVec.setComponent(j, Math.random() * 2 * Math.PI);
     }
     marker.rotation.setFromVector3(rotVec);
 
-    // we need to store a reference to the objects since we are going to query later
-    let meshes = marker.children;
-    for (let i = 0; i < meshes.length; ++i) {
-      octree.add(meshes[i]);
-    }
+    // We need to store a reference to the meshes in the group since we are going to query later
+    marker.children.forEach( function (mesh) {
+      octree.add(mesh);
+    });
+
     scene.add(marker);
   }
 
-  selectedObject = null;
-  auxMaterial = null;
+  selectedGroup = null;
+  auxMaterials = [];
   // create lights (Three point lighting and ambient)
   const ambientLight = new THREE.AmbientLight(0x303030);
   const keyLight = new THREE.PointLight(0xffffff);
@@ -163,11 +159,7 @@ function init() {
   // Add the mouse click event
   window.addEventListener( 'click', onClick, false);
 
-  // Add OrbitControls so that we can pan around with the mouse.
-  orbitCtrl = new THREE.OrbitControls(camera, renderer.domElement);
-
   // The manipulation controls init logic
-  manipulCtrl = new THREE.TransformControls( camera, renderer.domElement );
   manipulCtrl.addEventListener( 'change', render );
 
   manipulCtrl.addEventListener( 'dragging-changed', function ( event ) {
@@ -181,14 +173,14 @@ function pickingLogic() {
   raycaster.setFromCamera(mouse, camera);
   // calculate objects intersecting the picking ray
   // Query the octree to see which objects are near the ray
-  let closeObjects = octree.search( raycaster.ray.origin, raycaster.ray.far, true, raycaster.ray.direction );
+  const closeObjects = octree.search( raycaster.ray.origin, raycaster.ray.far, true, raycaster.ray.direction );
   // only search on the potential close objects
-  let intersections = raycaster.intersectOctreeObjects(closeObjects);
+  const intersections = raycaster.intersectOctreeObjects(closeObjects);
   // see if we hit something
   if (intersections.length > 0) {
     console.log('Hit!');
-    // is a new selection?
-    if (intersections[0].object.parent != selectedObject) {
+    // is a new selection? Select the whole group not just the intersection object
+    if (intersections[0].object.parent != selectedGroup) {
       console.log('New selection!');
       selectLogic(intersections[0].object.parent);
     }
@@ -198,19 +190,26 @@ function pickingLogic() {
 
 }
 
-function selectLogic(newObject) {
-  if (newObject === null) {
+function selectLogic(newGroup) {
+  if (newGroup === null) {
     console.log('Impossible to select null object!');
   }
   // Return previous selected object to original material
-  if (selectedObject !== null) {
-    selectedObject.material = auxMaterial;
+  if (selectedGroup !== null) {
+    selectedGroup.material = auxMaterials;
+    for (let i = 0; i < auxMaterials; ++i) {
+      selectedGroup.children[i].material = auxMaterials[i];
+    }
   }
   // update selection
-  selectedObject = newObject;
-  auxMaterial = selectedObject.material;
-  // Change the current selected object material
-  selectedObject.material = matSelected;
+  selectedGroup = newGroup;
+  selectedGroup.children.forEach( function(mesh) {
+    // Store the current selected object's material
+    auxMaterials.push(mesh.material);
+    // Change the current selected object's material
+    mesh.material = matSelected;
+  })
+
   // add manipulation controls to object
   if (manipulCtrl !== null) {
     if (transformMode == 0) {
@@ -219,7 +218,7 @@ function selectLogic(newObject) {
       manipulCtrl.setMode('rotate');
     }
     scene.add(manipulCtrl);
-    manipulCtrl.attach(newObject);
+    manipulCtrl.attach(newGroup);
   }
 }
 
